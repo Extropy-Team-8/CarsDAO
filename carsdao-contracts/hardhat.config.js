33de2c579b2dcb60729ae9d52c8f648200daf2b2
Christian <christian.honkanen@gmail.com>
2022-01-25 00:45:54 +0000

    config files

--- carsdao-contracts/hardhat.config.js
+++ carsdao-contracts/hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,10 +20,29 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.6",
+  networks: {
+    avalancheTest: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      gasPrice: 225000000000,
+      gas: 3000000,
+      chainId: 43113,
+      accounts: [process.env.PRIV_KEY],
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIV_KEY],
+      gasPrice: 12000000000,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      rinkeby: process.env.ETHERSCAN_KEY,
+    },
+  },
   settings: {
     optimizer: {
       runs: 200,
-      enabled: true
-    }
-  }
+      enabled: true,
+    },
+  },
 };
